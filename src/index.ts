293aeb987a9a34e724cd7ee5dc1e4a4f85952752
bobbyhadz <opmftw@gmail.com>
2024-02-28 18:54:17 +0200

    minor change

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+export {};
+
+const emp: Employee = {
+  id: 1,
+  name: 'Bobby Hadz',
+  salary: 100,
+};
+
+console.log(emp);
+
+const person: Person = {
+  name: 'Tom',
+  age: 30,
+};
+
+console.log(person);
